497b6560a6149cd4e80fe9662096f138de749cb7
vietcheems <viet.pq194463@sis.hust.edu.vn>
2021-04-07 22:39:15 +0700

    commented the old swap method

---
+++ Lab02/AimsProject/src/TestPassingParameter.java
@@ -0,0 +1,32 @@
+
+public class TestPassingParameter {
+	public static void main(String[] args) {
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		
+		swap(jungleDVD, cinderellaDVD);
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		System.out.println("cinderelaa dvd title: " + cinderellaDVD.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+	}
+	
+//	public static void swap(Object o1, Object o2) {
+//		Object tmp = o1;
+//		o1 = o2;
+//		o2 = tmp;
+//	}
+	
+	public static void changeTitle(DigitalVideoDisc dvd, String title) {
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
+	}
+	
+	public static void swap(DigitalVideoDisc d1, DigitalVideoDisc d2) {
+		String tmp = d1.getTitle();
+		changeTitle(d1, d2.getTitle());
+		changeTitle(d2, tmp);
+	}
+}
