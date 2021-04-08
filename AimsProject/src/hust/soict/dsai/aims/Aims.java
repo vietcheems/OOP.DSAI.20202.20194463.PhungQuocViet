3bf1ef9b8787c2687ac495092dc7014f7ee2e613
vietcheems <73997794+vietcheems@users.noreply.github.com>
2021-04-08 22:03:33 +0700

    idk whats going on

---
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -0,0 +1,29 @@
+package hust.soict.dsai.aims;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+public class Aims {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cart anOrder = new Cart();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+				"Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+				"Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+				"Animation", 18.99f);
+		
+		DigitalVideoDisc[] dvdList = {dvd1, dvd2, dvd3};
+		anOrder.addDigitalVideoDisc(dvdList);
+
+
+		
+		System.out.println("Total Cost is: ");
+		System.out.println(anOrder.totalCost());
+		
+		anOrder.removeDigitalVideoDisc(dvd2);
+		System.out.println("Total Cost after removing: ");
+		System.out.println(anOrder.totalCost());
+	}
+}
