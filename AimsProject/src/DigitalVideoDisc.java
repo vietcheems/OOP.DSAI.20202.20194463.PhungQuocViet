aa880e4fe3c8f9412f422acc1f78bf9ccc2683a3
vietcheems <73997794+vietcheems@users.noreply.github.com>
2021-05-04 00:09:45 +0700

    added javafx exercise

---
+++ AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,48 @@
+
+public class DigitalVideoDisc {
+	private String title;
+	private String catogory;
+	private String director;
+	private int length;
+	private float cost;
+	public String getTitle() {
+		return title;
+	}
+	public String getCatogory() {
+		return catogory;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	public DigitalVideoDisc(String title, String catogory, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.catogory = catogory;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String catogory, String director, float cost) {
+		super();
+		this.title = title;
+		this.catogory = catogory;
+		this.director = director;
+		this.cost = cost;
+	}
+	public DigitalVideoDisc(String title, String catogory, float cost) {
+		super();
+		this.title = title;
+		this.catogory = catogory;
+		this.cost = cost;
+	}
+}
