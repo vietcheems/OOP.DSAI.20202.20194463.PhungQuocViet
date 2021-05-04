1a48851c1414967287e6b614ef788e746c36df53
vietcheems <73997794+vietcheems@users.noreply.github.com>
2021-05-04 16:37:29 +0700

    added media

--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -3,29 +3,42 @@ package hust.soict.dsai.aims.media;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Book {
-	 private int id;
-	 private String title;
-	 private String category;
-	 private float cost;
-	 private List<String> authors = new ArrayList<String>();
-	public Book() {
-		// TODO Auto-generated constructor stub
-	}
-	public int getId() {
-		return id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public float getCost() {
-		return cost;
-	}
+public class Book extends Media{
+	private List<String> authors = new ArrayList<String>();
+
 	public List<String> getAuthors() {
 		return authors;
 	}
+	
+	public boolean addAuthor(String authorName) {
+		int index = authors.indexOf(authorName);
+		if (index == -1) {
+			authors.add(authorName);
+			System.out.println("Author added successfully");
+			return true;
+		}
+		else {
+			System.out.println(authorName + " is already an author of " + title);
+			return false;
+		}
+	}
+	public boolean removeAuthor(String authorName) {
+		int index = authors.indexOf(authorName);
+		if (index == -1) {
+			System.out.println(authorName + " is not an author of " + title);
+			return false;
+		}
+		else {
+			authors.remove(index);
+			System.out.println("Author removed successfully");
+			return true;
+		}
+	}
 
+	public Book(int id, String title, String category, float cost, String dataAdded, List<String> authors) {
+		super(id, title, category, cost, dataAdded);
+		this.authors = authors;
+	}
+	
+	
 }
--- AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -1,5 +1,5 @@
 package hust.soict.dsai.test.store;
-import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
 import hust.soict.dsai.aims.store.Store;
 
 public class StoreTest {
