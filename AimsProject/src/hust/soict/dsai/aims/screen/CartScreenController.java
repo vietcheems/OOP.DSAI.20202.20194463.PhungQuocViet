982a36efa7e8550144b84e9cfc1554388e8929a4
vietcheems <73997794+vietcheems@users.noreply.github.com>
2021-05-20 15:49:04 +0700

    added filter feature

--- AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -5,15 +5,30 @@ import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.Playable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CartScreenController {
 	
 	private Cart cart;
+	private FilteredList<Media> filteredData;
+	private boolean filterById = true;
+	
+	@FXML
+	private TextField tfFilter;
+	
+	@FXML
+	private RadioButton radioBtnFilterId;
+	
+	@FXML
+	private RadioButton radioBtnFilterTitle;
 	
     @FXML
     private Button btnPlay;
@@ -61,6 +76,36 @@ public class CartScreenController {
 					}
 				}
 			});
+		
+		tfFilter.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue,
+					String newValue) {
+				showFilteredMedia(newValue);
+			}
+		});
+	}
+	
+	void showFilteredMedia(String inputString) {
+		filteredData = new FilteredList<>(this.cart.getItemsOrdered());
+		filteredData.setPredicate(media -> {
+				if (filterById) {
+					if (String.valueOf(media.getId()).indexOf(inputString) != -1) {
+						return true;
+					} else
+						return false;
+				}
+				else {
+					String filterString = inputString.toLowerCase();
+					if (filterString == null || filterString.isBlank()) {
+						return true;
+					}
+					else if (media.getTitle().toLowerCase().indexOf(filterString) != -1)
+						return true;
+					else return false;
+				}
+			});
+		tblMedia.setItems(filteredData);
 	}
 	
 	void updateButtonBar(Media media) {
@@ -73,4 +118,19 @@ public class CartScreenController {
 		}
 	}
 	
+	@FXML
+	void btnRemovePressed(ActionEvent event) {
+		Media media = tblMedia.getSelectionModel().getSelectedItem();
+		cart.removeMedia(media);
+	}
+	
+    @FXML
+    void radioBtnFilterIdPressed(ActionEvent event) {
+    	filterById = true;
+    }
+
+    @FXML
+    void radioBtnFilterTitlePressed(ActionEvent event) {
+    	filterById = false;
+    }
 }
